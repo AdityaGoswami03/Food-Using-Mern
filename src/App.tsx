d7adce70df8bc9207f57dbe40378f2bfe3947e81
Aditya Goswami <adityagoswami327@gmail.com>
2025-09-04 08:46:59 +0530

    Navbar UI change ls3:20.12

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 // import { useState } from 'react'
 
 // import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./screen/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // ✅ for toggler & dropdowns
+import Login from "./screen/Login";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Router>
       <div>
-        <Home></Home>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
--- src/screen/Home.tsx
+++ src/screen/Home.tsx
@@ -7,6 +7,21 @@ export default function Home() {
       <div>
         <Navbar />
       </div>
+      <div>
+        <div
+          className="card mt-3"
+          style={{ width: "18rem", maxHeight: "330px" }}
+        >
+          <img src="..." className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card’s content.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
