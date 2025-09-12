7da0d5eb3da8fa9ad568f1835666f0035676d52f
Aditya Goswami <adityagoswami327@gmail.com>
2025-09-12 14:38:09 +0530

    database connection error fixed

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,16 @@
-// getting-started.js
 const mongoose = require("mongoose");
 
-main().catch((err) => console.log(err));
-const dataBaseUri =
-  "mongodb+srv://AdityaGoswami03:Aditya%4003@cluster0.3qndeud.mongodb.net/";
-async function main() {
-  await mongoose.connect(dataBaseUri);
-  console.log("Connected with the data");
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-}
+const uri =
+  "mongodb+srv://AdityaGoswami03:Aditya%4003@cluster0.3qndeud.mongodb.net/mydb?retryWrites=true&w=majority&appName=Cluster0";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(uri);
+    console.log("✅ MongoDB Atlas connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 9090;
 
+const connectDB = require("./db");
+connectDB();
+
 app.get("/", (req, res) => {
   res.send("this is response");
 });
